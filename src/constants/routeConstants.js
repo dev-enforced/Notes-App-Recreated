86327fcf60a1e98d86e6ef0dc824e322e9b0c55f
Roshan J <devenforced@gmail.com>
2022-08-03 20:39:39 +0530

    [refactor]: constants for different types of files

---
+++ src/constants/routeConstants.js
@@ -0,0 +1,11 @@
+const routeConstants = {
+  HOME_ROUTE: "/",
+  SIGNUP: "/signup",
+  LOGIN: "/login",
+  NOTES_HOME: "/noteshome",
+  NOTES_ARCHIVES: "/notesarchives",
+  NOTES_TRASH: "/notestrash",
+  MOCKBEE: "/mockbee",
+  MOCKMAN: "/mockman",
+};
+export { routeConstants };
